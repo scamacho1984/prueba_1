26d3ee67b79deaaab0674d265f6592ea1fd14591
Sergio Camacho <sergiocamacho1984@gmail.com>
2021-04-08 01:05:21 -0500

    Maquetacion del tema 4

--- src/views/curso/Tema4.vue
+++ src/views/curso/Tema4.vue
@@ -1,136 +1,68 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(titulo="4. Área de Reservas")
+  BannerInterno(titulo="4. Estructura organizacional.")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    p La misión del hotel es garantizar un servicio de alojamiento personalizado con calidad, comodidad y seguridad en un entorno confortable, siempre con el objetivo del bienestar y la satisfacción de las necesidades de los huéspedes.
+
+    p Por tal razón se requiere de una estructura que permita lograr el objetivo. La estructura organizacional es el modo en que una empresa planifica y distribuye su trabajo, cada empresa tiene su propia estructura.
+
     figure.mb-5
-      img(src="@/assets/curso/tema4/tema4-main.jpg")
+      img(src="@/assets/curso/tema4/tema4-main.png")
 
-    .row.justify-content-between
-      .col-12.col-md-6.my-3.my-md-0
-        p El área de reservas tiene en la gran mayoría de los casos el primer contacto con los clientes que en un futuro inmediato visitarán el hotel; manteniendo  contacto permanente con los diferentes tipos de clientes: agencias de viajes, empresas y particulares.
+    p Los hoteles se estructuran con la sectorización de las labores en distintas unidades llamadas departamentos. Cada departamento atiende un conjunto específico de necesidades del cliente.
 
-        p Este departamento se encarga de administrar de manera eficiente la disponibilidad de habitaciones, procurando siempre venderlas de la forma más conveniente y rentable para el hotel.
+    p La representación gráfica de <strong>la estructura organizacional de una empresa se conoce como organigrama,</strong> no existe un organigrama que indique la distribución de los departamentos, y que sea válido para todos los hoteles, ya que esto depende de la capacidad y la categoría del hotel, de las características físicas de la edificación, del tipo de organización y de servicio que se desea proporcionar. También se presentan organigramas para áreas específicas.
 
-      .col-12.col-md-5.my-3.my-md-0
-        blockquote.fw-light.fs-3.p-4.bg-color-sistema-g  Las reservas pueden ser telefónicas, directas, virtuales, por grupos y a través, de sistemas de reservaciones o GDS.
+    .custom-alert.container.tarjeta.tarjeta__template--verde.px-3.pt-3.pb-2.px-sm-5.my-5
+      .ms-lg-3.ps-lg-5
+        p.ms-lg-5 <strong>Aquí encontraras los tres ejemplos de modelos de estructura de organigrama.</strong>
+      .float-image.d-none.d-lg-block
+        img.auto(src="@/assets/curso/tema3/importante.png")
 
-    h2.h3#t_4_1 4.1 Funciones
-    figure.my-5
-      img(src="@/assets/curso/tema3/video.jpg")
+    p Gráficamente en general se presenta de la siguiente forma:
 
-    p El personal que trabaja en reservas debe saber vender los servicios que ofrece a los clientes y poseer los conocimientos necesarios que implica el proceso de reservaciones.
+    p Los puestos de trabajo y los procesos están sometidos a una reglamentación y estandarización los cuales son respaldados por manuales de funciones y procesos:
 
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Recepción y control
-      p Tiene como principal función la recepción y control de las solicitudes de espacio para la reserva de habitaciones, tanto  de las agencias de viajes, como de otras empresas y público en general, además cumple con una importante labor en el proceso de  venta de habitaciones, manteniendo  actualizada  la recepción, respecto a la situación ocupacional del hotel, con el objetivo de captar la mayor cantidad de clientes.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Primer contacto
-      p Es el primer contacto de los clientes con el hotel y debe proporcionar una imagen de amabilidad en el trato y eficiencia en la prestación de servicio.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Gestión eficiente
-      p Gestión eficiente de la disponibilidad de habitaciones, revisando constantemente el nivel de ocupación y realizando previsiones adecuadas para evitar situaciones conflictivas tales como sobreocupación.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Resolver
-      p Resolver de forma ágil y con disposición  cualquier solicitud de alojamiento por parte de un cliente.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Toma de reservas
-      p Toma de reservas individuales y de grupos.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Gestión de cupos
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Planning
-      p Elaboración y archivo de documentación (planning, hoja y libro de reservas).
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Previsión
-      p Previsión de servicios y comunicación con los demás  departamentos del hotel.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Atendiende las peticiones
-      p Realiza reservas de habitaciones, se procede, atendiendo las peticiones de los clientes según sus preferencias y su histórico de estancias.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Recibe el rooming
-      p Recibe el rooming list que las agencias de viajes envían asegurándose que estos contengan la mayor cantidad de información  posible y que ésta no varíe.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Elabora la lista
-      p Elabora la lista de llegadas previstas y oficializada con la firma del responsable de esta actividad.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Archiva
-      p Archiva toda la información por un período de tiempo determinado.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Informa a la recepción
-      p Informa a la recepción de cualquier cambio en la lista de llegadas previstas del día, después que esta haya sido emitida, siempre con el tiempo necesario para su tratamiento.
-    .tarjeta--color-curso-g.border.border-5.px-5.pt-3.pb-1.my-3
-      .h6 Comunica al cliente
-      p En aquellos casos en los que no exista disponibilidad de alojamiento para las fechas solicitadas, igualmente se le comunica al cliente, dejando abierta la posibilidad de comunicarle disponibilidad de alojamiento en caso de anulación o cancelación de alguna otra reserva previamente concedida, en cuyo caso lo anotaremos como reserva en lista de espera.
-
-    h2.h3#t_4_2 4.2 Cualidades del personal de reservas
-    .row.mt-5
-     .col-12.col-md-6.my-3
-      img(src="@/assets/curso/tema4/cualidades1.jpg")
-      p.px-3 <strong>Excelente presentación</strong> personal acorde a las normas de etiqueta y protocolo (aseo personal, portar adecuadamente el uniforme, buena postura)
-     .col-12.col-md-6.my-3
-      img(src="@/assets/curso/tema4/cualidades2.jpg")
-      p.px-3 <strong>Amabilidad y cortesía</strong>, ser respetuoso de la necesidad de las personas, actuando con buena actitud y muy receptivo.
-     .col-12.col-md-6.my-3
-      img(src="@/assets/curso/tema4/cualidades3.jpg")
-      p.px-3 <strong>Comunicación verbal</strong>, vocabulario sencillos, comprensibles y apropiados, hablar fluida y tranquilamente, evitando respuestas monosílabas.
-     .col-12.col-md-6.my-3
-      img(src="@/assets/curso/tema4/cualidades4.jpg")
-      p.px-3 <strong>Comunicación no verbal</strong>, cuidar movimientos corporales, su expresión facial, comunicación visual y táctil. Manejar la distancia proxémica con el huésped.
-
-    h2.h3#t_4_3 4.3 Sistemas de reserva
     .row.justify-content-between.mt-5.align-items-end
       .col-12.col-md-6
-        p Existen diversas modalidades de los sistemas de reservas, conocidos generalmente como CRS (Computer Reservation Systems) y los sistemas de distribución global, conocidos generalmente como GDS (Global Distribution Systems). 
+        p <strong>Manual de funciones:</strong> es un instrumento de trabajo que contiene el conjunto de normas y tareas que desarrolla cada funcionario en sus actividades cotidianas y es elaborado técnicamente basados en los respectivos procedimientos, sistemas y normas del establecimiento.
 
-        p Se trata de <strong>sistemas basados en la informática y en la interconexión y registro de datos que permiten la realización de reservas desde los diferentes puntos de venta</strong>. Su grado de implantación en las reservas hoteleras no es tan amplio como en el relativo a las reservas aéreas, donde han encontrado su mayor representación.
+        p Son guías y orientaciones para desarrollar las rutinas o labores cotidianas de cada uno de los trabajadores quienes podrán tomar las decisiones más acertadas apoyadas por las directrices de los superiores, y estableciendo con claridad la responsabilidad y las obligaciones que cada uno de los cargos conlleva.
 
-        p Los sistemas de distribución  global <strong>ofrecen una extensa selección de productos de viaje, incluyendo líneas aéreas, hoteles, trenes, cruceros, seguros de viajes, alquiler de coches, tours, arrendadoras de autos, agencias de viajes</strong>, entre otros.
+        p Incluye brevemente cada una de las funciones y responsabilidades básicas describiendo adecuadamente los límites y la esencia del cargo.
 
       .col-12.col-md-5
-        img(src="@/assets/curso/tema4/sitemas-reservas.jpg")
+        img(src="@/assets/curso/tema4/funcion-1.png")
+    .row.justify-content-between.mt-5.align-items-end
+      .col-12.col-md-5
+        img(src="@/assets/curso/tema4/funcion-2.png")
+      .col-12.col-md-6
+        p <strong>Manual de procedimientos:</strong> el manual de procedimientos es "un instrumento de apoyo en el que se encuentran de manera sistemática los pasos a seguir, para ejecutar las actividades de un puesto determinado y/o funciones de la unidad administrativa". (Gómez, 2001).
 
-    p Los principales son:
+        p Se puede decir entonces que es un documento de apoyo para el personal de una empresa, el cual contiene políticas y detalles de las actividades a realizarse en funciones específicas.
+    .row.justify-content-between.mt-5.align-items-end
+      .col-12.col-md-6
+        p <strong>Formatos:</strong>  el formato es el conjunto de las características técnicas y de presentación de un texto, objeto o documento en distintos ámbitos, tanto reales como virtuales.
 
-    Slider#slider-tema1
-      .tarjeta.tarjeta__template--azul-claro.px-5.py-4
-          .row.align-items-center.px-lg-5
-            .col-12.col-md-3
-              img.auto(src="@/assets/curso/tema4/sistema1.png")
-            .col-12.col-md
-              p <strong>SABRE</strong> (The Sabre Group)
-              p Proveedor de soluciones tecnológicas, proveedor de soluciones tecnológicas de información para la industria del transporte y los viajes. Implantado sobre todo en Estados Unidos.
-      .tarjeta.tarjeta__template--azul-claro.px-5.py-4
-          .row.align-items-center.px-lg-5
-            .col-12.col-md-3
-              img.auto(src="@/assets/curso/tema4/sistema2.png")
-            .col-12.col-md
-              p <strong>APOLLO</strong>
-              p Principal impulsor del sistema Galileo en América y ubicado principalmente en Estados Unidos.
-      .tarjeta.tarjeta__template--azul-claro.px-5.py-4
-          .row.align-items-center.px-lg-5
-            .col-12.col-md-3
-              img.auto(src="@/assets/curso/tema4/sistema3.png")
-            .col-12.col-md
-              p <strong>AMADEUS</strong>
-              p Un GDS que proporciona herramientas de distribución, ventas y mercadotecnia a los profesionales de los viajes en todo el mundo. Es una compañía establecida en Madrid, España, propiedad de Iberia, Lufthansa, Air France, Continental Airlines.  En España la compañía es SAVIA.
-      .tarjeta.tarjeta__template--azul-claro.px-5.py-4
-          .row.align-items-center.px-lg-5
-            .col-12.col-md-3
-              img.auto(src="@/assets/curso/tema4/sistema4.png")
-            .col-12.col-md
-              p <strong>WORLDSPAN</strong>
-              p GDS que proporciona comunicación y distribución electrónica de información a más de 500 proveedores líderes mundiales en viajes. Sus dueños son Delta Air Lines, Northwets Airline, Transworld  Airlines, se ubica en Estados Unidos.
+        p Sirven para:
 
-    p.my-5 Los GDS, permiten llevar una buena gestión de reservas y ofrecen la posibilidad de:
-    ul.ms-4
-      li Realizar reservas individuales o grupales según tarifa disponible y tipo de habitación.
-      li Transferir los datos del historial del cliente a la reserva.
-      li Guardar mensajes para clientes que aún no han llegado.
-      li Modificar reservas, reflejando los motivos, fechas del cambio y quien fue la persona que lo solicito.
-      li Agrupar reservas ya realizadas.
-      li Asignación automática de habitaciones.
-      li Confirmación automática de reservas en lista de espera.
-      li Lista de habitaciones disponibles.
+        ul.ms-4
+          li El control y mantenimiento de los servicios que se prestan.
+          li Apoyan a controlar la calidad y resultado que se ofrece en el establecimiento.
+          li Para planificar, controlar y evaluar el trabajo.
+          li Para la garantía de la eficiencia del servicio.
+          li Optimización de la prestación del servicio.
+
+      .col-12.col-md-5
+        img(src="@/assets/curso/tema4/funcion-3.png")
+
+    .custom-alert.container.tarjeta.tarjeta__template--verde.px-3.pt-3.pb-2.px-sm-5.my-5
+      .float-image.d-none.d-lg-block
+        img.auto(src="@/assets/curso/tema3/importante.png")
+      .ms-lg-3.ps-lg-5
+        p.ms-lg-5 <strong>En el establecimiento hotelero se utilizan para todas las áreas cada uno con su fin, algunos son comunes a todas las áreas: ejemplo: “Lista de huéspedes”</strong>
+
+
 
 </template>
 
